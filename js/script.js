a07ccfaebf5a4d9e35b18954b906be3d70213275
dax <audaxvillegas3@gmail.com>
2023-11-15 19:44:43 +0800

    script.js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Jack, the friendly neighbor,
+//waved at Dax as he walked by Dax's house.
+//for a cup of coffee.
+
+let p1 = "JackPogi";
+let p2 = "DaxTangkad";
+let pronoun = "he";
+let drink = "kape";
+
+alert( p1+", the friendly neighbor, waved at " + p2+ ", as " + pronoun +" walked by "+p2+" 's house. " + p2 + " smiled back and invited " + p1 + " in for a cup of " + drink);
+
+// let firstName = "Dax";
+// let pi = 3.14;
+
+// alert("Hi, " + firstName);
+// alert("How are you " + firstName + "?");
+// alert(firstName + ", The value of pi is " + pi);
+
+// alert(firstName);
+
+
+// alert("Hello " + "Dax");
+
+// let $name = "Dax";
+
+// $name = "Jack";
+// alert($name);
+
+//document.getElementById("text").innerHTML = $name;
+// console.log($name);
+// alert($name);
+
+// console.log("Hello World");
+// alert("Hello World");
