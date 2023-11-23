417a67293023efc148e43bc97215f54805cc680e
dax <audaxvillegas3@gmail.com>
2023-11-23 12:07:21 +0800

    script3.js

---
+++ js/script3.js
@@ -0,0 +1,12 @@
+// let num1 = "88.88";
+// num1 = parseFloat (num1);
+// console.log();
+
+// let num1 = "23";
+// let num2 = "51";
+// console.log (parseInt(num1) + num2);
+
+let x = 12;
+let y = 3;
+console.log(x/y+x);
+
